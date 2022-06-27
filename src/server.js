d89ea5e83ea5e726eddd0f73f1102403a83b6f22
타래 <85001878+meowTarae@users.noreply.github.com>
2022-06-27 17:15:57 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import express from "express";
 
-const PORT = 3000;
+const PORT = 4000;
 
 const app = express(); //  create express application
 
 const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
-app.listen(3000, handleListening);
+app.listen(PORT, handleListening);
 // 유저의 request를 받아들임, 받아들일 port와 callback을 받음
 // callback은 몇번 포트를 listening해줄지
