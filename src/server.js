1d52016cf7d830729cf145444eae9f0b6c0b4478
타래 <85001878+meowTarae@users.noreply.github.com>
2022-06-28 20:34:57 +0900

    .

--- src/server.js
+++ src/server.js
@@ -4,8 +4,14 @@ const PORT = 4000;
 
 const app = express(); //  create express application
 
+const handleHome = (req, res) => {
+  return res.end("<h1>Homepage</h1>");
+};
+
+app.get("/", handleHome);
+
 const handleListening = () =>
-  console.log(`Server listening on port http://localhost:${PORT}`);
+  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
 
 app.listen(PORT, handleListening);
 // 유저의 request를 받아들임, 받아들일 port와 callback을 받음
