68f7aa468dc5e3c62b9361dbbe5ea376bafa2fb7
타래 <85001878+meowTarae@users.noreply.github.com>
2022-06-30 22:27:38 +0900

    0630

--- src/server.js
+++ src/server.js
@@ -1,31 +1,36 @@
 import express from "express";
 import morgan from "morgan";
-
 const PORT = 4000;
 
 const app = express(); //  create express application
 const logger = morgan("dev");
-
-const handleHome = (req, res) => {
-  console.log(`Method : ${req.method}, URL : ${req.url}`);
-  return res.end("<h1>Homepage</h1>");
-};
-
-const protectController = (req, res, next) => {
-  const URL = req.url;
-  if (URL === "/protect") {
-    return res.send("<h1>You can't access this page.</h1>");
-  }
-  next();
-};
-
 app.use(logger);
-app.use(protectController);
-app.get("/", handleHome);
+
+const globalRouter = express.Router();
+
+const handleHome = (req, res) => res.send("Home");
+
+globalRouter.get("/", handleHome);
+
+const userRouter = express.Router();
+
+const handleEditUser = (req, res) => res.send("Edit User");
+
+userRouter.get("/edit", handleEditUser);
+
+const videoRouter = express.Router();
+
+const handleWatchVideo = (req, res) => res.send("Watch Video");
+
+videoRouter.get("/watch", handleWatchVideo);
+
+app.use("/", globalRouter);
+app.use("/videos", videoRouter);
+app.use("/users", userRouter);
 
 const handleListening = () =>
-  console.log(`✅ Server listening on port http://localhost:${PORT} 🚀`);
-
+  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 app.listen(PORT, handleListening);
+
 // 유저의 request를 받아들임, 받아들일 port와 callback을 받음
 // callback은 몇번 포트를 listening해줄지
