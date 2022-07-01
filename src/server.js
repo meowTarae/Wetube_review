2463257eae1a771fdd3c51696691bc1997a57193
타래 <85001878+meowTarae@users.noreply.github.com>
2022-07-01 20:40:26 +0900

    0701

--- src/server.js
+++ src/server.js
@@ -1,28 +1,14 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import videoRouter from "./routers/videoRouter";
+import userRouter from "./routers/userRouter";
+
 const PORT = 4000;
 
-const app = express(); //  create express application
+const app = express();
 const logger = morgan("dev");
 app.use(logger);
-
-const globalRouter = express.Router();
-
-const handleHome = (req, res) => res.send("Home");
-
-globalRouter.get("/", handleHome);
-
-const userRouter = express.Router();
-
-const handleEditUser = (req, res) => res.send("Edit User");
-
-userRouter.get("/edit", handleEditUser);
-
-const videoRouter = express.Router();
-
-const handleWatchVideo = (req, res) => res.send("Watch Video");
-
-videoRouter.get("/watch", handleWatchVideo);
 
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
@@ -30,7 +16,5 @@ app.use("/users", userRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
-app.listen(PORT, handleListening);
 
-// 유저의 request를 받아들임, 받아들일 port와 callback을 받음
-// callback은 몇번 포트를 listening해줄지
+app.listen(PORT, handleListening);
