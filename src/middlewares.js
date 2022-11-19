40f25514667a0d9852673a6eb045699e40bfc945
타래 <85001878+meowTarae@users.noreply.github.com>
2022-11-20 00:01:24 +0900

    Wetube ~#8.9

--- src/middlewares.js
+++ src/middlewares.js
@@ -1,12 +1,10 @@
 import multer from "multer";
-
 export const localsMiddleware = (req, res, next) => {
   res.locals.loggedIn = Boolean(req.session.loggedIn);
   res.locals.siteName = "Wetube";
   res.locals.loggedInUser = req.session.user || {};
   next();
 };
-
 export const protectorMiddleware = (req, res, next) => {
   if (req.session.loggedIn) {
     return next();
@@ -14,7 +12,6 @@ export const protectorMiddleware = (req, res, next) => {
     return res.redirect("/login");
   }
 };
-
 export const publicOnlyMiddleware = (req, res, next) => {
   if (!req.session.loggedIn) {
     return next();
@@ -23,4 +20,15 @@ export const publicOnlyMiddleware = (req, res, next) => {
   }
 };
 
-export const uploadFiles = multer({ dest: "uploads/" });
+export const avatarUpload = multer({
+  dest: "uploads/avatars/",
+  limits: {
+    fileSize: 3000000,
+  },
+});
+export const videoUpload = multer({
+  dest: "uploads/videos/",
+  limits: {
+    fileSize: 10000000,
+  },
+});
