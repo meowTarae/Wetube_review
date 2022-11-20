7086e0c119bd5d212960102ae3ba30d656df0b2a
타래 <85001878+meowTarae@users.noreply.github.com>
2022-11-21 02:26:19 +0900

    Wetube ~#8.15

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,5 @@
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
-
 const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   avatarUrl: String,
@@ -9,9 +8,14 @@ const userSchema = new mongoose.Schema({
   password: { type: String },
   name: { type: String, required: true },
   location: String,
+  videos: [{ type: mongoose.Schema.Types.ObjectId, ref: "Video" }],
 });
+
 userSchema.pre("save", async function () {
-  this.password = await bcrypt.hash(this.password, 5);
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 5);
+  }
 });
+
 const User = mongoose.model("User", userSchema);
 export default User;
