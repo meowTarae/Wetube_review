23d0c0381bcd6494c29a5ba451b4f83e6fd11511
타래 <85001878+meowTarae@users.noreply.github.com>
2022-11-22 15:41:19 +0900

    Fixed home's avatar img
    홈 화면에서 사진 안나오는거 수정

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -199,6 +199,9 @@ export const see = async (req, res) => {
       model: "User",
     },
   });
+  console.log(user);
+  console.log("아래는 dir");
+  console.dir(user);
   if (!user) {
     return res.status(404).render("404", { pageTitle: "User not found." });
   }
