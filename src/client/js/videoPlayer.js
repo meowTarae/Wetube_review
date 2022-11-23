2e1fa3ca2207c325a13dac0b1931a3aeecf3c97e
타래 <85001878+meowTarae@users.noreply.github.com>
2022-11-24 05:51:48 +0900

    Wetube ~#11.11

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,13 +1,22 @@
 const video = document.querySelector("video");
 const playBtn = document.getElementById("play");
+const playBtnIcon = playBtn.querySelector("i");
 const muteBtn = document.getElementById("mute");
+const muteBtnIcon = muteBtn.querySelector("i");
 const volumeRange = document.getElementById("volume");
 const currenTime = document.getElementById("currenTime");
 const totalTime = document.getElementById("totalTime");
 const timeline = document.getElementById("timeline");
+const fullScreenBtn = document.getElementById("fullScreen");
+const fullScreenIcon = fullScreenBtn.querySelector("i");
+const videoContainer = document.getElementById("videoContainer");
+const videoControls = document.getElementById("videoControls");
 
+let controlsTimeout = null;
+let controlsMovementTimeout = null;
 let volumeValue = 0.5;
 video.volume = volumeValue;
+let finish = false;
 
 const handlePlayClick = (e) => {
   if (video.paused) {
@@ -15,7 +24,7 @@ const handlePlayClick = (e) => {
   } else {
     video.pause();
   }
-  playBtn.innerText = video.paused ? "Play" : "Pause";
+  playBtnIcon.classList = video.paused ? "fas fa-play" : "fas fa-pause";
 };
 const handleMuteClick = (e) => {
   if (video.muted) {
@@ -23,7 +32,9 @@ const handleMuteClick = (e) => {
   } else {
     video.muted = true;
   }
-  muteBtn.innerText = video.muted ? "Unmute" : "Mute";
+  muteBtnIcon.classList = video.muted
+    ? "fas fa-volume-mute"
+    : "fas fa-volume-up";
   volumeRange.value = video.muted ? 0 : volumeValue;
 };
 const handleVolumeChange = (event) => {
@@ -51,6 +62,8 @@ const handleLoadedMetadata = () => {
 const handleTimeUpdate = () => {
   currenTime.innerText = formatTime(Math.floor(video.currentTime));
   timeline.value = Math.floor(video.currentTime);
+
+  if (video.currentTime === video.duration) finish = true;
 };
 
 const handleTimelineChange = (event) => {
@@ -59,14 +72,48 @@ const handleTimelineChange = (event) => {
   } = event;
   video.currentTime = value;
 };
+const handleTimelineChanged = (event) => {
+  if (video.paused && finish) {
+    video.play();
+    playBtn.innerText = "Pause";
+    finish = false;
+  }
+};
+const handleFullscreen = () => {
+  const fullscreen = document.fullscreenElement;
+  if (fullscreen) {
+    document.exitFullscreen();
+    fullScreenIcon.classList = "fas fa-expand";
+  } else {
+    videoContainer.requestFullscreen();
+    fullScreenIcon.classList = "fas fa-compress";
+  }
+};
 
-if (video.readyState >= 2) {
-  getMetadata();
-}
+const hideControls = () => videoControls.classList.remove("showing");
+const handleMouseMove = () => {
+  if (controlsTimeout) {
+    clearTimeout(controlsTimeout);
+    controlsTimeout = null;
+  }
+  if (controlsMovementTimeout) {
+    clearTimeout(controlsMovementTimeout);
+    controlsMovementTimeout = null;
+  }
+  videoControls.classList.add("showing");
+  controlsMovementTimeout = setTimeout(hideControls, 2000);
+};
 
-function getMetadata() {
-  handleLoadedMetadata();
-}
+const handleMouseLeave = () => {
+  controlsTimeout = setTimeout(hideControls, 2000);
+};
+
+const handleVideoClick = () => handlePlayClick();
+
+const handleUserKeydown = (event) => {
+  const { code } = event;
+  if (code === "Space") handlePlayClick();
+};
 
 playBtn.addEventListener("click", handlePlayClick);
 muteBtn.addEventListener("click", handleMuteClick);
@@ -74,3 +121,17 @@ volumeRange.addEventListener("input", handleVolumeChange);
 video.addEventListener("loadedmetadata", handleLoadedMetadata);
 video.addEventListener("timeupdate", handleTimeUpdate);
 timeline.addEventListener("input", handleTimelineChange);
+timeline.addEventListener("change", handleTimelineChanged);
+fullScreenBtn.addEventListener("click", handleFullscreen);
+videoContainer.addEventListener("mousemove", handleMouseMove);
+videoContainer.addEventListener("mouseleave", handleMouseLeave);
+video.addEventListener("click", handleVideoClick);
+window.addEventListener("keydown", handleUserKeydown);
+
+/* video.duration 안보일때 */
+if (video.readyState >= 2) {
+  getMetadata();
+}
+function getMetadata() {
+  handleLoadedMetadata();
+}
